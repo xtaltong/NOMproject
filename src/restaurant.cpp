704d9ebfd3158b8189b0333b3aedb7c3a58dcdac
ECheng742 <echen111@ucr.edu>
2020-11-22 17:30:43 -0800

    added restaurant class - add, remove, get items and display order

---
+++ src/restaurant.cpp
@@ -0,0 +1,22 @@
+#include "../header/item.hpp"
+#include "../header/restaurant.hpp"
+#include <iostream>
+using namespace std;
+
+Restaurant::Restaurant() {}
+
+void Restaurant::addItem(Item* item) {
+    order.push_back(item);
+}
+
+void Restaurant::removeItem(Item* item) {
+
+}
+
+Item* Restaurant::getItem(int index) const {
+    return order.at(index);
+}
+
+void Restaurant::display() const {
+    
+}
