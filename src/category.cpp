42b4ffd2914316c41bc48bddac56df9a1888de97
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2020-12-05 21:52:07 -0800

    Added functionality to category for the tagging system

--- src/category.cpp
+++ src/category.cpp
@@ -16,7 +16,38 @@ void Category::removeTag(Tag* tag) {
 }
 
 void Category::display() const {
+    cout << name;
     for (auto i : tags) {
+        cout << ", ";
         i->display();
+        
     }
 }
+
+// {side}
+// {American, fried}
+//             {potato}
+//                 fries
+// side 
+// American, fried
+//             potato
+//                 fries
+
+// Fries: $1.00
+// Tags (5): side, American, fried, potato, (fries)
+
+// side, American, fried, potato, fries
+// Tag* t = new Category("side");
+// Tag* a = new Category("American");
+// Tag* f = new Category("fried");
+// Tag* p = new Category("potato");
+// Tag* i = new FoodItem("fries");
+// t->addTag(a);
+// t->addTag(f);
+// f->addTag(p);
+// p->addTag(i);
+// t->display();
+
+// Item* item = new Item("fries");
+// item.addTag("side")
+// item.addTag("American", "side")
