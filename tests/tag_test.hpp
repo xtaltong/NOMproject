23c01f1611d3e9a33f9bd305b422f89a2352913b
ECheng742 <echen111@ucr.edu>
2020-12-10 15:29:57 -0800

    Implemented tests for fooditem and category classes

--- tests/tag_test.hpp
+++ tests/tag_test.hpp
@@ -7,15 +7,35 @@
 #include "../header/fooditem.hpp"
 #include "../header/category.hpp"
 
+TEST(FoodItemTest, FoodItemTest) {
+	Tag* test = new FoodItem("Chicken Sandwich");
+	Tag* newtag = new FoodItem("Drink");
+	EXPECT_EQ(test->getName(), "Chicken Sandwich");
+	test->display();
+	cout << endl;
+	test->addTag(newtag);
+	EXPECT_EQ(test->find("Chicken Sandwich"), nullptr);
+}
+
+TEST(CategoryTest, BasicCategoryTest) {
+	Tag* test = new Category("Sandwich");
+	test->display();
+	EXPECT_EQ(test->getName(), "Sandwich");
+}
+
+TEST(CategoryTest, ComplexCategoryTest) {
+	Tag* test = new Category("Sandwich");
+	EXPECT_EQ(test->find("Sandwich"), nullptr);
+}
+  
+/*
 TEST(TagTest, BasicTagTest) {
     Restaurant* test = new Restaurant("Chick-Fil-A");
     Item* item1 = new Item("Chicken Sandwich", 5.99);
     Item* item2 = new Item("Medium Drink", 1.99);
     test->addItem(item1);
     test->addItem(item2);
-    EXPECT_EQ(test->getItem(0), item1);
-    test->removeItem(item1);
-    EXPECT_EQ(test->getItem(0), item2);
+    item1->addFoodTag();
 }
-
+*/
 #endif //__TAG_TEST_HPP__
