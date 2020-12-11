a8569c0ca4b185523bdf8acfc5ea8b3fa0bdbf53
ECheng742 <echen111@ucr.edu>
2020-12-11 11:19:32 -0800

    stash

--- header/ddpricebreakdown.hpp
+++ header/ddpricebreakdown.hpp
@@ -1,13 +1,14 @@
-#ifndef __DDPRICEBREAKDOWN_HPP__
+/*#ifndef __DDPRICEBREAKDOWN_HPP__
 #define __DDPRICEBREAKDOWN_HPP__
 
 #include <iostream>
 #include <vector>
 #include "pricebreakdown.hpp"
-class PriceBreakdown;
+
 class DDPriceBreakdown : public PriceBreakdown {
         public:
 		DDPriceBreakdown();
                 virtual void collectPriceMetadata(Restaurant *);
 };
 #endif
+*/
