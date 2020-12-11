964d8d9d802d27d302b3d85b4d4e2e260447dd56
ECheng742 <56359858+ECheng742@users.noreply.github.com>
2020-12-11 03:13:33 -0800

    Update factory.cpp

--- src/factory.cpp
+++ src/factory.cpp
@@ -13,6 +13,105 @@ UberEatsFactory::UberEatsFactory(Restaurant* rest) : Factory(rest) {}
 
 void UberEatsFactory::createPriceBreakdown() {
     ifstream fin;
+    fin.open("../CSV/UberEats - Restauraunt List.csv");
+
+    if(!fin.is_open()){
+        throw runtime_error("Could not open File");
+    }
+    vector<string> row; 
+    string line, word, temp, colname;
+
+    while(fin >> temp){
+        row.clear();
+        getline(fin,line);
+        istringstream ss(line);
+        //checks first phrase in each column
+        while(getline(ss, colname, ',')){
+            //if name matches restaurant name, push row info into vector row
+            if (colname == rest->getRestaurantName()){
+                while(ss >> word){
+                    row.push_back(word);    
+                    if(ss.peek() == ',') ss.ignore();
+                }
+            }
+        }
+    }
+
+    fin.close();
+    
+    double subtotal = rest->getSubtotal();
+    //small order fee
+    double smallFee = 3;
+    //if subtotal(before tax) < $15, apply small order fee
+    if(subtotal < 15){
+        Fee small("Small Order Fee", smallFee);
+        feeBreakdown.push_back(small);
+    }
+    double tFee = 0.0872*subtotal;
+    //tax = 108.72 x cost
+    Fee tax("Tax", tFee);
+    feeBreakdown.push_back(tax);
+    //service fee = handling x num Items
+     //service fee = 15%    
+    double servFee = subtotal * 1.15;
+    Fee serviceFee("Service", servFee);
+    feeBreakdown.push_back(serviceFee);
+    //delivery Fee = fee x distance
+    double dFee = stod(row[2]) * stod(row[1]);
+    Fee deliveryFee("Delivery", dFee);
+    feeBreakdown.push_back(deliveryFee);
+    // pbd = new UEPriceBreakdown();
+    // return pbd;
+}
+
+TimeBreakdown* UberEatsFactory::createTimeBreakdown() {
+    ifstream fin;
+    fin.open("../CSV/UberEats - Restauraunt List.csv");
+
+    if(!fin.is_open()){
+        throw runtime_error("Could not open File");
+    }
+    vector<string> row; 
+    string line, word, temp, colname;
+
+    while(fin >> temp){
+        row.clear();
+        getline(fin,line);
+        istringstream ss(line);
+        //checks first phrase in each column
+        while(getline(ss, colname, ',')){
+            //if name matches restaurant name, push row info into vector row
+            if (colname == rest->getRestaurantName()){
+                while(ss >> word){
+                    row.push_back(word);    
+                    if(ss.peek() == ',') ss.ignore();
+                }
+            }
+        }
+    }
+
+    fin.close();
+    
+    // calculate time needed to prepare food
+    int foodPrep = rest->getOrder().size() * 2;
+    Time cooking("Cooking", foodPrep);
+    timeBreakdown.push_back(cooking);
+
+    int deliveryTime = stoi(row[4]);
+    Time delivery("Delivery", deliveryTime);
+    timeBreakdown.push_back(delivery);
+    // tbd = new UETimeBreakdown();
+    // return tbd;
+}
+
+// void UberEatsFactory::createRestaurantList() {
+
+// }
+
+DoorDashFactory::DoorDashFactory(Restaurant* rest) : Factory(rest) {}
+
+void DoorDashFactory::createPriceBreakdown() {
+    ifstream fin;
     fin.open("../CSV/DoorDash - Restauraunt List.csv");
 
     if(!fin.is_open()){
@@ -73,29 +172,57 @@ void UberEatsFactory::createPriceBreakdown() {
     double dFee = stod(column[2]) * stod(column[1]);
     Fee deliveryFee("Delivery", dFee);
     feeBreakdown.push_back(deliveryFee);
-    // pbd = new UEPriceBreakdown();
+    // pbd = new DDPriceBreakdown();
     // return pbd;
 }
 
-TimeBreakdown* UberEatsFactory::createTimeBreakdown() {
-    tbd = new UETimeBreakdown();
-    return tbd;
-}
-
-// void UberEatsFactory::createRestaurantList() {
-
-// }
-
-DoorDashFactory::DoorDashFactory(Restaurant* rest) : Factory(rest) {}
-
-void DoorDashFactory::createPriceBreakdown() {
-    pbd = new DDPriceBreakdown();
-    return pbd;
-}
-
 TimeBreakdown* DoorDashFactory::createTimeBreakdown() {
-    tbd = new DDTimeBreakdown();
-    return tbd;
+    ifstream fin;
+    fin.open("../CSV/DoorDash - Restauraunt List.csv");
+
+    if(!fin.is_open()){
+        throw runtime_error("Could not open File");
+    }
+    vector<string> column; 
+    string line, word, temp;
+    getline(fin, line);
+    istringstream ss(line);
+    int colIdx = 0;
+    while(ss >> word){
+        if(word == rest->getRestaurantName()){
+            column.push_back(word);
+            break;
+        }
+        else{
+            if(ss.peek() == ',') ss.ignore();
+            colIdx++;
+        }
+    }
+    while(getline(fin,line)){
+        int columnIdx = 0;
+        while(ss >> word){
+            if(columnIdx == colIdx){
+                column.push_back(word);
+                if(ss.peek() == ',') ss.ignore();
+            }
+            else{
+                columnIdx++;
+            }
+        }
+    }
+
+    fin.close();
+
+    // calculate time needed to prepare food
+    int foodPrep = (rest->getOrder().size()) * 2;
+    Time cooking("Cooking", foodPrep);
+    timeBreakdown.push_back(cooking);
+
+    int deliveryTime = stoi(column[4]);
+    Time delivery("Delivery", deliveryTime);
+    timeBreakdown.push_back(delivery);
+    // tbd = new DDTimeBreakdown();
+    // return tbd;
 }
 
 // void DoorDashFactory::createRestaurantList() {
